38e11ac568b6d9733b9f18bbc021d38146a05e29
Komal-preet-Kaur <komalpreetkaur18278@gmail.com>
2024-10-28 20:57:05 +0530

    Day16

---
+++ Day16/query_selector.js
@@ -0,0 +1,4 @@
+let doc=document.querySelectorAll("p");
+console.dir(doc); 
+let heading=document.querySelector("h1");
+console.log(heading);
---
+++ Day16/string_concat.js
@@ -0,0 +1,4 @@
+let opt1='English';
+let opt2='Science';
+let res=opt1+opt2;
+console.log(res);
