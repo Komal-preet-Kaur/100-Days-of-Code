89ae2384d912f55219cf33d5e0bcd6d5fece642e
Komal-preet-Kaur <komalpreetkaur18278@gmail.com>
2024-12-07 12:54:37 +0530

    Day56

---
+++ Day56/date.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n==17){
+		    System.out.println("YAY");
+		}
+		else{
+		    System.out.println("NO");
+		}
+		sc.close();
+	}
+}
