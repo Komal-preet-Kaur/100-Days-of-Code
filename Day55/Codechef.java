c218853b9ada26502db045bf59267689d7d9c52e
Komal-preet-Kaur <komalpreetkaur18278@gmail.com>
2024-12-07 12:54:24 +0530

    Day55

---
+++ Day55/Codechef.java
@@ -0,0 +1,18 @@
+package Day55;
+import java.util.Scanner;
+
+
+public class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();  
+        if (x > 35) {  
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+         sc.close();
+    }
+}
