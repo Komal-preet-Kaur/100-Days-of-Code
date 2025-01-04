1b33f3ac8f527ca79b6d4f6a1f8cd20d50a224c6
Komal-preet-Kaur <komalpreetkaur18278@gmail.com>
2025-01-04 22:50:41 +0530

    Day84

---
+++ Day84/Head.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Head {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();  
+        sc.nextLine();  
+        while (t-- > 0) {
+            int n = sc.nextInt();  
+            sc.nextLine();  
+            String str = sc.nextLine();  
+            
+            if (str.contains("I")) {
+                System.out.println("INDIAN");
+            } else if (str.contains("Y")) {
+                System.out.println("NOT INDIAN");
+            } else {
+                System.out.println("NOT SURE");
+            }
+        }
+        sc.close();
+    }
+}
